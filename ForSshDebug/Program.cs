0cf95b292dc8d436b4ece9970840c5eddd38d6c2
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2023-06-07 12:27:26 +0300

    init commit

---
+++ ForSshDebug/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ForSshDebug
+{
+    internal class Program
+    {
+        private static int I;
+        public static void Main(string[] args)
+        {
+            I = 0;
+            while (true)
+            {
+                Console.WriteLine(I++);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
